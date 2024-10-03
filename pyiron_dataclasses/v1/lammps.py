5af05b30f1610839fa570e60cbdc1f05b285ed5b
Jan Janßen <janssen@mpie.de>
2024-10-03 11:32:30 +0200

    initial commit

---
+++ pyiron_dataclasses/v1/lammps.py
@@ -0,0 +1,54 @@
+from dataclasses import dataclass
+from typing import Optional, List
+
+
+from pyiron_dataclasses.v1.generic import (
+    GenericDict,
+    Interactive,
+    GenericInput,
+    GenericOutput,
+    Executable,
+    Server,
+    Structure,
+)
+
+
+@dataclass
+class LammpsPotential:
+    citation: Optional[str]
+    config: List[str]
+    filename: List[str]
+    model: str
+    name: str
+    species: List[str]
+
+
+@dataclass
+class LammpsInputFiles:
+    control_inp: str
+    potential_inp: str
+
+
+@dataclass
+class LammpsInput:
+    generic_dict: GenericDict
+    interactive: Interactive
+    generic: GenericInput
+    structure: Structure
+    potential: LammpsPotential
+    input_files: LammpsInputFiles
+
+
+@dataclass
+class LammpsOutput:
+    generic: GenericOutput
+
+
+@dataclass
+class LammpsJob:
+    calculation_input: LammpsInput
+    executable: Executable
+    server: Server
+    calculation_output: LammpsOutput
+    status: str
+    job_id: int
