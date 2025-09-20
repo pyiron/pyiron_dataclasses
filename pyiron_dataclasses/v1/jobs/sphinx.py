a31723599df7aef931ff7ce5db2ae9a1aba61594
Jan Janssen <jan-janssen@users.noreply.github.com>
2025-09-20 13:39:59 +0200

    Refactor internal structure to separate dataclasses from converter (#68)
    * Refactor internal structure to separate dataclasses from converter
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add more tests
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- pyiron_dataclasses/v1/jobs/sphinx.py
+++ pyiron_dataclasses/v1/jobs/sphinx.py
@@ -3,16 +3,16 @@ from typing import List, Optional
 
 import numpy as np
 
-from pyiron_dataclasses.v1.atomistic import (
+from pyiron_dataclasses.v1.jobs.atomistic import (
     GenericInput,
     GenericOutput,
     Structure,
 )
-from pyiron_dataclasses.v1.dft import (
+from pyiron_dataclasses.v1.jobs.dft import (
     ChargeDensity,
     ElectronicStructure,
 )
-from pyiron_dataclasses.v1.job import (
+from pyiron_dataclasses.v1.jobs.generic import (
     Executable,
     GenericDict,
     Interactive,
